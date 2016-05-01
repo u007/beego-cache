03f04c9d99d31fb40eef781357ffa20ecd065fa3
James <james@mercstudio.com>
2016-05-02 00:22:38 +0800

    fixed function name for public use

--- beego_cache.go
+++ beego_cache.go
@@ -23,15 +23,15 @@ const PREFIX = "[ CACHE ] "
 func GetCache() *Cache {
   if instantiated == nil {
     instantiated = new(Cache);
-    instantiated.cacheInit()
+    instantiated.CacheInit()
   }
   return instantiated;
 }
 
-func (this *Cache) fileChanged(path string) bool {
-	cache_size, cache_time, cache_new_file, err := this.fileCacheStat(path)
+func (this *Cache) FileChanged(path string) bool {
+	cache_size, cache_time, cache_new_file, err := this.FileCacheStat(path)
 	if err != nil {
-		beego.Debug("filecachestat error:", err.Error())
+		beego.Debug("Filecachestat error:", err.Error())
 		return true
 	}
 	
@@ -53,22 +53,22 @@ func (this *Cache) fileChanged(path string) bool {
 	return false
 }
 
-func (this *Cache) fileCacheStat(path string) (file_size int64, file_modtime time.Time, file_dest string, err error) {
+func (this *Cache) FileCacheStat(path string) (file_size int64, file_modtime time.Time, file_dest string, err error) {
 	name := fmt.Sprintf("file_%s", path)
-	if this.cache_exists(name) {
+	if this.CacheExists(name) {
 		// beego.Debug("Cache exists", name)
-		cache := this.cache_get(name)
+		cache := this.CacheGet(name)
 		res   := strings.Split(cache, "|")
 		// beego.Debug(fmt.Sprintf("result: %q", res))
 		file_size, err := strconv.ParseInt(res[0], 10, 64)
 		if (err != nil) {
-			Warning("fileCacheStat: can't get size from cache for %s", path)
+			Warning("FileCacheStat: can't get size from cache for %s", path)
 		}
 		// beego.Debug("cache:", cache)
 		// https://gobyexample.com/time-formatting-parsing
 		file_modtime, err := time.Parse(time.RFC3339, res[1]) //TODO
 		if (err != nil) {
-			Warning("fileCacheStat: mod time from cache: %s: %s", res[1], err.Error())
+			Warning("FileCacheStat: mod time from cache: %s: %s", res[1], err.Error())
 		}
 		file_dest  := res[2]
 		return file_size, file_modtime, file_dest, nil
@@ -78,15 +78,15 @@ func (this *Cache) fileCacheStat(path string) (file_size int64, file_modtime tim
 	}
 }
 
-func (this *Cache) cacheFile(path string, stat os.FileInfo, new_file_path string) {
+func (this *Cache) CacheFile(path string, stat os.FileInfo, new_file_path string) {
 	name := fmt.Sprintf("file_%s", path)
-	err := this.cache_set_max(name, fmt.Sprintf("%d|%s|%s", stat.Size(), stat.ModTime().Format(time.RFC3339), new_file_path))
+	err := this.CacheSetMax(name, fmt.Sprintf("%d|%s|%s", stat.Size(), stat.ModTime().Format(time.RFC3339), new_file_path))
 	if err != nil {
 		beego.Debug("error set cacheFile", err.Error())
 	}
 }
 
-func (this *Cache) cacheInit() error {
+func (this *Cache) CacheInit() error {
 	if this.cache == nil {
 		bm, err := cache.NewCache("redis", `{"conn":":6379"}`)  
 		this.cache = bm
@@ -98,28 +98,28 @@ func (this *Cache) cacheInit() error {
 	return nil
 }
 
-func (this *Cache) cache_name(name string) string {
+func (this *Cache) CacheName(name string) string {
 	res := strings.Replace(name, "/", ":", -1)
 	return res
 }
 
-func (this *Cache) cache_get(name string) string {
-	result := fmt.Sprintf("%s", this.cache.Get(this.cache_name(name)))
+func (this *Cache) CacheGet(name string) string {
+	result := fmt.Sprintf("%s", this.cache.Get(this.CacheName(name)))
 	// beego.Debug("cache_get", name, fmt.Sprintf("%q", result))
 	return result
 }
 
-func (this *Cache) cache_set(name string, value string, timeout time.Duration) error {
-	err := this.cache.Put(this.cache_name(name), value, timeout)
+func (this *Cache) CacheSet(name string, value string, timeout time.Duration) error {
+	err := this.cache.Put(this.CacheName(name), value, timeout)
 	return err
 }
 
-func (this *Cache) cache_set_max(name string, value string) error {
-	return this.cache_set(name, value, 60 * 60 * 24 * 365 * time.Second)
+func (this *Cache) CacheSetMax(name string, value string) error {
+	return this.CacheSet(name, value, 60 * 60 * 24 * 365 * time.Second)
 }
 
-func (this *Cache) cache_exists(name string) bool {
-	return this.cache.IsExist(this.cache_name(name))
+func (this *Cache) CacheExists(name string) bool {
+	return this.cache.IsExist(this.CacheName(name))
 }
 
 func Warning(format string, v... interface{}) {
